c7422c8328942d8911799639e1429c5baa788bb9
Anh Pha <anhphapap0@gmail.com>
2025-04-19 16:12:14 +0700

    change mysql db

--- vaccineapis/vaccineapp/vaccineapp/settings.py
+++ vaccineapis/vaccineapp/vaccineapp/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import dj_database_url
 from pathlib import Path
 from decouple import config
 
@@ -29,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Application definition
 
