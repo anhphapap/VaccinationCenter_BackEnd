45953875305349668d22761fe5adf4c33c3d9383
godreplyme <nguyenhovu0984@gmail.com>
2025-04-19 15:40:29 +0700

    update requirements

--- vaccineapis/vaccineapp/vaccineapp/settings.py
+++ vaccineapis/vaccineapp/vaccineapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,16 +84,12 @@ WSGI_APPLICATION = 'vaccineapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vaccinedb',
-        'USER': 'root',
-        'PASSWORD': 'vrain2403',
-        'HOST': ''
-    }
-}
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
