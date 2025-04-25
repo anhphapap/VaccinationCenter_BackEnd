31c939609b469b15caa2af5dacab13a6bd8e070f
Anh Pha <anhphapap0@gmail.com>
2025-04-25 20:44:23 +0700

    add CSRF verification

--- vaccineapis/vaccineapp/vaccineapp/settings.py
+++ vaccineapis/vaccineapp/vaccineapp/settings.py
@@ -16,6 +16,9 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://vaccinationcenterbackend-production.up.railway.app",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
