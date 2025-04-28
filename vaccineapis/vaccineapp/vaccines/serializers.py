6f579a1884ff1eb86cb2b5f7fa3bee092eaab63a
Anh Pha <anhphapap0@gmail.com>
2025-04-28 10:41:34 +0700

    add full field user serializer

--- vaccineapis/vaccineapp/vaccines/serializers.py
+++ vaccineapis/vaccineapp/vaccines/serializers.py
@@ -38,8 +38,9 @@ class VaccineDetailSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name',
-                  'username', 'email', 'password', 'avatar']
+        fields = ['id', 'first_name', 'last_name', 'username',
+                    'email', 'password', 'avatar',
+                    'phone', 'address', 'birth_date', 'gender', 'is_completed_profile']
         extra_kwargs = {
             'password': {'write_only': True}
         }
