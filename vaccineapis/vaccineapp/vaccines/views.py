4a9c8d227344b81ebe49790631a60a270a571e13
godreplyme <nguyenhovu0984@gmail.com>
2025-05-17 17:18:18 +0700

    add vnpay

--- vaccineapis/vaccineapp/vaccines/views.py
+++ vaccineapis/vaccineapp/vaccines/views.py
@@ -432,47 +432,44 @@ def hmacsha512(key, data):
 @api_view(['POST'])
 def payment(request):
     if request.method == 'POST':
-        # Process input data and build url payment
-        form = PaymentForm(request.POST)
-        if form.is_valid():
-            order_type = form.cleaned_data['order_type']
-            order_id = request.data.get('order_id')
-            amount = request.data.get('amount')
-            order_desc = request.POST.get('order_desc')
-            bank_code = form.cleaned_data['bank_code']
-            language = form.cleaned_data['language']
-            ipaddr = get_client_ip(request)
-            # Build URL Payment
-            vnp = vnpay()
-            vnp.requestData['vnp_Version'] = '2.1.0'
-            vnp.requestData['vnp_Command'] = 'pay'
-            vnp.requestData['vnp_TmnCode'] = settings.VNPAY_TMN_CODE
-            vnp.requestData['vnp_Amount'] = amount * 100
-            vnp.requestData['vnp_CurrCode'] = 'VND'
-            vnp.requestData['vnp_TxnRef'] = order_id
-            vnp.requestData['vnp_OrderInfo'] = order_desc
-            vnp.requestData['vnp_OrderType'] = order_type
-            # Check language, default: vn
-            if language and language != '':
-                vnp.requestData['vnp_Locale'] = language
-            else:
-                vnp.requestData['vnp_Locale'] = 'vn'
-                # Check bank_code, if bank_code is empty, customer will be selected bank on VNPAY
-            if bank_code and bank_code != "":
-                vnp.requestData['vnp_BankCode'] = bank_code
-
-            vnp.requestData['vnp_CreateDate'] = datetime.now().strftime(
-                '%Y%m%d%H%M%S')  # 20150410063022
-            vnp.requestData['vnp_IpAddr'] = ipaddr
-            vnp.requestData['vnp_ReturnUrl'] = settings.VNPAY_RETURN_URL
-            vnpay_payment_url = vnp.get_payment_url(
-                settings.VNPAY_PAYMENT_URL, settings.VNPAY_HASH_SECRET_KEY)
-            print(vnpay_payment_url)
-            return JsonResponse({'payment_url': vnpay_payment_url})
+    # Process input data and build url payment        if form.is_valid():
+        order_type = request.data['order_type']
+        order_id = request.data.get('order_id')
+        amount = request.data.get('amount')
+        order_desc = request.data.get('order_desc')
+        bank_code = request.data.get['bank_code']
+        language = request.data.get['language']
+        ipaddr = get_client_ip(request)
+        # Build URL Payment
+        vnp = vnpay()
+        vnp.requestData['vnp_Version'] = '2.1.0'
+        vnp.requestData['vnp_Command'] = 'pay'
+        vnp.requestData['vnp_TmnCode'] = settings.VNPAY_TMN_CODE
+        vnp.requestData['vnp_Amount'] = amount * 100
+        vnp.requestData['vnp_CurrCode'] = 'VND'
+        vnp.requestData['vnp_TxnRef'] = order_id
+        vnp.requestData['vnp_OrderInfo'] = order_desc
+        vnp.requestData['vnp_OrderType'] = order_type
+        # Check language, default: vn
+        if language and language != '':
+            vnp.requestData['vnp_Locale'] = language
         else:
-            print("Form input not validate")
+            vnp.requestData['vnp_Locale'] = 'vn'
+            # Check bank_code, if bank_code is empty, customer will be selected bank on VNPAY
+        if bank_code and bank_code != "":
+            vnp.requestData['vnp_BankCode'] = bank_code
+
+        vnp.requestData['vnp_CreateDate'] = datetime.now().strftime(
+            '%Y%m%d%H%M%S')  # 20150410063022
+        vnp.requestData['vnp_IpAddr'] = ipaddr
+        vnp.requestData['vnp_ReturnUrl'] = settings.VNPAY_RETURN_URL
+        vnpay_payment_url = vnp.get_payment_url(
+            settings.VNPAY_PAYMENT_URL, settings.VNPAY_HASH_SECRET_KEY)
+        print(vnpay_payment_url)
+        return JsonResponse({'payment_url': vnpay_payment_url})
     else:
-        return render(request, "payment/payment.html", {"title": "Thanh toán"})
+        print("Form input not validate")
+
 
 
 def payment_ipn(request):
